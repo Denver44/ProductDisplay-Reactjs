531d6048873d7dbb0eddb8c3e1134fd01dbbe517
Durgesh <durgeshrai2012@gmail.com>
2021-03-12 16:35:32 +0530

    minor change

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,34 +1,37 @@
 import React from "react";
 import "./product.css";
 import { useSelector } from "react-redux";
-
 function Product() {
   const item = useSelector((state) => state.selectedProduct);
 
   return (
     <div className="product">
-      <div className="product__details" key={item._id}>
-        <div
-          className="product__img"
-          style={{ backgroundImage: ` url(${item.src})` }}
-        />
+      {item ? (
+        <div className="product__details" key={item._id}>
+          <div
+            className="product__img"
+            style={{ backgroundImage: ` url(${item.src})` }}
+          />
 
-        <div className="product__box">
-          <div className="product__boxRow">
-            <h2>{item.title}</h2>
-            <p> ₹{item.price}</p>
+          <div className="product__box">
+            <div className="product__boxRow">
+              <h2>{item.title}</h2>
+              <p> ₹{item.price}</p>
+            </div>
+            <div className="product__rating">
+              {Array(item.rating)
+                .fill()
+                .map((_, i) => {
+                  return <p>⭐</p>;
+                })}
+            </div>
+            <p>{item.description}</p>
+            <button className="product__boxCart">Add to cart</button>
           </div>
-          <div className="product__rating">
-            {Array(item.rating)
-              .fill()
-              .map((_, i) => {
-                return <p>⭐</p>;
-              })}
-          </div>
-          <p>{item.description}</p>
-          <button className="product__boxCart">Add to cart</button>
         </div>
-      </div>
+      ) : (
+        <h1>Go Back and Select One Item</h1>
+      )}
     </div>
   );
 }
